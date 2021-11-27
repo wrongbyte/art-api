5de214746b03733bd7bf6ce89909f2cbbc2d1f1f
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2021-11-27 17:05:07 -0300

    change folder structure1

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new Pool({
-    connectionString,
-  });
-
-module.exports = () => { return pool; }
