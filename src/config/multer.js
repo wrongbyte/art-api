745e1e9f7d4a752d9ce2735c6a4b186f38ea4acc
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2021-12-16 12:23:32 -0300

    add multer verification

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,6 +1,18 @@
 const multer = require('multer');  
-const uploadImage = multer({storage:
-  multer.memoryStorage()
+const { BadRequestError } = require('../utils/errors');
+const uploadImage = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 1024 * 1024 * 10
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg'){
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new BadRequestError('Format not allowed. Please submit a png, jpg or jpeg file.'));
+    }
+  }
 });
 
 module.exports = uploadImage;
